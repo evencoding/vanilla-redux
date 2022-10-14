a2ed5d0be48880aae01bbb4f9ac5500cacd71d44
evencoding <evencoding@gmail.com>
2022-10-14 19:45:35 +0900

    React Setup

---
+++ src/routes/Home.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const Home = () => {
+  const [text, setText] = useState("");
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(text);
+  };
+  return (
+    <div>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+      </form>
+      <ul></ul>
+    </div>
+  );
+};
+
+export default Home;
